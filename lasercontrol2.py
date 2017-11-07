f855b703fc7f744adab05d0f35c489dd2554c2a5
Dylan Armitage <d.armitage89@gmail.com>
2017-11-07 10:36:29 -0800

    Initial change to tkinter instead of curses

--- lasercontrol2.py
+++ lasercontrol2.py
@@ -10,7 +10,7 @@ access to the laser, then let the user actually do so.
 
 
 ####---- Imports ----####
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 from subprocess import check_output
 
@@ -19,13 +19,16 @@ import os
 import signal
 import pwd
 import time
-import curses
-import textwrap
+#import textwrap
 import random
 import crypt
+import ttk
 
+try:
+    import Tkinter as tk
+except ImportError:
+    import tkinter as tk
 import wiringpi as GPIO
-
 
 ####---- Variables ----####
 # BCM pins for various functions
@@ -37,21 +40,73 @@ OUT_PINS = dict(laser=20, psu=21, grbl=27)
 ## Sensors and other inputs
 IN_PINS = dict() # None currently
 
+
+####---- Classes ----####
+class MainWindow(tk.Frame):
+    """Main window"""
+    def __init__(self, root, *args, **kwargs):
+        tk.Frame.__init__(self, root, *args, **kwargs)
+        self.root = root
+        self.root.geometry("400x200+0-5")
+        self.root.resizable(width=False, height=False)
+        self.grid()
+
+        self.gpio = tk.Frame(self.root)
+        self.gpio.grid(column=10, row=0, sticky="E", ipadx=10)
+
+        self.gcode = tk.Frame(self.root)
+        self.gcode.grid(column=0, row=0, sticky="W", ipadx=10)
+
+        self.gpio.button_auth = ttk.Button(self.gpio, text="Authorize")
+        self.gpio.button_psu = ttk.Button(self.gpio, text="Power Supply")
+        self.gpio.button_laser = ttk.Button(self.gpio, text="Laser")
+        self.gpio.button_reset_hard = ttk.Button(self.gpio, text="Hard Reset")
+
+        self.gcode.button_start = ttk.Button(self.gcode, text="Start")
+        self.gcode.button_pause = ttk.Button(self.gcode, text="Pause")
+        self.gcode.button_stop = ttk.Button(self.gcode, text="Stop")
+
+        self.__init_window()
+
+    def __init_window(self):
+        """Initialization of the GUI"""
+        self.root.title("Laser Control")
+        self.__create_buttons()
+
+    def __create_buttons(self):
+        """Create GPIO gpio"""
+        self.gpio.button_auth.grid(row=10)
+        self.gpio.button_psu.grid(row=20)
+        self.gpio.button_laser.grid(row=30)
+        self.gpio.button_reset_hard.grid(row=40)
+
+        self.gcode.button_start.grid(column=10, row=10)
+        self.gcode.button_pause.grid(column=20, row=10)
+        self.gcode.button_stop.grid(column=30, row=10)
+
+
+
 ####---- Generic Functions ----####
 ### NFC-related
-def initialize_nfc_reader(stdscr):
+def initialize_nfc_reader():
     """Verify that correct board is present, return firmware ver & name.
+
     The function name is leftover from the initial function which used
     Adafruit_PN532 to get the NFC data."""
-    raw_lines = check_output(["/usr/bin/nfc-scan-device", "-v"]).split("\n")
-    try:
-        chip_line = [line for line in raw_lines if "chip:" in line][0]
-        chip_name = chip_line.split(" ")[1]
-        chip_firm = chip_line.split(" ")[2]
-    except IndexError:
-        msg = "No board present!"
-        error_message(stdscr, msg)
-        chip_name, chip_firm = None, None
+    for _ in range(3):
+        # Do this 3 times, because sometimes the board doesn't
+        # wake up fast enough...
+        raw_lines = check_output(["/usr/bin/nfc-scan-device", "-v"])
+        raw_lines = raw_lines.split("\n")
+        try:
+            chip_line = [line for line in raw_lines if "chip:" in line][0]
+            chip_name = chip_line.split(" ")[1]
+            chip_firm = chip_line.split(" ")[2]
+            break
+        except IndexError:
+            time.sleep(1)
+    else:
+        chip_firm, chip_name = None, None
     return (chip_firm, chip_name)
 
 ## NFC UID get
@@ -188,183 +243,23 @@ def toggle_pin(pin):
     switch_pin(pin)
 
 ####---- Text functions ----####
-def error_message(stdscr, error):
-    """(curses.window, message string) -> None
+def error_message(_, __):
+    """Deal with error messages"""
+    #TODO
+    pass
 
-    This function should take in (essentially) an error message string
-    and do a little "pop-up" style curses window with the error message.
-    The user can then either dismiss the error or halt the program.
-    """
-    curses.curs_set(0)
-    # Create sub-window
-    subscr = stdscr.subwin(10, 60, 5, 10)
-    subscr.bkgd(" ", curses.A_REVERSE)
-    subscr.box()
-    subscr.refresh()
-    # Display error text
-    text_frame(error, subscr)
-    subscr.refresh()
-    # List options
-    subscr.addstr(9, 9, "Continue (any)")
-    subscr.addstr(9, 41, "Quit (q)")
-    subscr.refresh()
-
-    response = subscr.getkey()
-    if response == "q":
-        raise KeyboardInterrupt
-    else:
-        subscr.erase()
-        subscr.bkgd(" ", curses.color_pair(0))
-        subscr.refresh()
-
-def verify_text_length(message_list, length=76):
-    """Takes in a list of messages, returns True if all items are appropriate
-    length, first item number and length otherwise"""
-    failures = []
-    for position, message in enumerate(message_list):
-        message_length = len(message)
-        if message_length > length:
-            failures.append((position, message_length))
-
-    if failures:
-        return failures
-    # No need for 'else' since it'll return no matter what
-    return True
-
-def text_horizontal_border(stdscr, line):
-    """Create horizontal text border on line. DEPRECATED"""
-    stdscr.hline(line, 0, "+", 80)
-    stdscr.hline(line, 1, "-", 78) # Cover middle with '-'
-
-def text_frame(message, stdscr, offset=0, mode=None):
-    """Takes in string and add them to the curses window, wrap as neccessary."""
-
-    for line_str in message.split("\n"):
-        message_list = textwrap.wrap(line_str, 76)
-        for line, text in enumerate(message_list):
-            if mode:
-                stdscr.addstr(offset + line + 1, 2, text, mode)
-            else:
-                stdscr.addstr(offset + line + 1, 2, text)
-        if not message_list: # If the list is empty, add 1 manually
-            offset += 1
-        else:
-            offset += len(message_list)
-
-def machine_status(stdscr, y_offset):
-    """Prints machine state, returns dictionary of number assignments"""
-    # Calculate the center of where each item will go
-    # Don't forget this this is essentially a fencepost problem
-    slices = len(OUT_PINS) + 1
-    x_max = stdscr.getmaxyx()[1]
-    x_location = [x*x_max/slices for x in range(1, slices+1)]
-    pin_disabled = [GPIO.digitalRead(pin) for pin in OUT_PINS.itervalues()]
-    # Print pin name and a number below it
-    stdscr.addstr(y_offset, x_max/3, "Press number to toggle status")
-    y_offset += 1
-    enumerated_items = dict(enumerate(OUT_PINS))
-    for place, item in enumerated_items.iteritems():
-        start_x = x_location[place] - len(item)/2 - 1
-        stdscr.addstr(y_offset,
-                      start_x,
-                      item,
-                      curses.color_pair(2+pin_disabled[place]))
-        stdscr.addstr(y_offset+1, x_location[place]-2, "({})".format(place))
-    return enumerated_items
+def machine_status():
+    """Something with the machine status"""
+    #TODO
+    pass
 
 ####---- MAIN ----####
-def main(stdscr):
-    """Main function. Run in curses.wrapper()"""
-
-    intro = "This program will allow you to change the state of the " \
-            "laser and PSU, and reset the GRBL board (to act as a reset " \
-            "button).\n\nSearching for NFC tag...\n\n\n"
-
-    # Success/failure color pairs
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-
-    stdscr.clear()
-    stdscr.resize(20, 80) # 20 rows, 80 cols
-    curses.curs_set(0) # Cursor should be invsisible
-
-    # Verify that reader exists, and setup GPIO pins
-    firmware, board_name = initialize_nfc_reader(stdscr)
-    if not (firmware and board_name):
-        msg = "Invalid firmware version and/or board type\n" \
-              "Firmware: {} \n Board name: {}".format(firmware, board_name)
-        error_message(stdscr, msg)
-        raise RuntimeError("PN532 board is having issues.")
-    board_setup = gpio_setup(stdscr)
-    if not board_setup:
-        error_message(stdscr, "GPIO pins failed to setup")
-        raise RuntimeError("GPIO pins failed to setup")
-
-    # Welcome, welcome, one and all...
-    text_frame(intro, stdscr)
-    stdscr.box()
-    stdscr.refresh()
-
-    y_offset, _ = stdscr.getyx()
-    while True:
-        user_id = get_uid_block(dummy=False)
-        nfc_id = "Your NFC UID is 0x{}, correct? [y/n]".format(user_id)
-        text_frame(nfc_id, stdscr, offset=y_offset)
-        stdscr.refresh()
-        response = stdscr.getkey()
-        if response == "y":
-            break
-
-    # Next: Verify UID (esp. that it belongs to the logged in user, then
-    # attempt to turn on PSU and laser.
-    # Make sure that the laser constantly requires the presence of the tag,
-    # otherwise turn it off.
-
-    y_offset, _ = stdscr.getyx()
-    while True:
-        # We don't except the user to ever have to press "n", since that
-        # would imply that there is something wrong with get_user_uid().
-        # TO DO: There might actually be something wrong with get_user_uid(),
-        # if two users have the same NFC tag. Although this may not be an
-        # actual problem, and just something that arises during testing.
-        username = get_user_uid(user_id)
-        user_string = "You are user {}, correct? [y/n]".format(username)
-        text_frame(user_string, stdscr, y_offset)
-        stdscr.refresh()
-        response = stdscr.getkey()
-        if response == "y":
-            if not is_current_user(username):
-                raise SystemExit("Invalid user+key")
-            break
-    # Make sure that they aren't using someone else's NFC tag
-    user_string = "{} ({})".format(get_user_realname(), username)
-    y_offset, _ = stdscr.getyx()
-    user_verified = verify_uid(user_id)
-    if user_verified:
-        text_frame(user_string, stdscr, -1, mode=curses.color_pair(2))
-    else:
-        raise SystemExit("Not authorized user")
-    text_frame("Let's get the laser going!", stdscr, y_offset)
-    stdscr.refresh()
-
-    while True:
-        assignments = machine_status(stdscr, 14)
-        stdscr.refresh()
-        response = stdscr.getkey()
-        try:
-            int_resp = int(response)
-            item = assignments[int_resp]
-            if item == 'grbl':
-                toggle_pin(OUT_PINS[item])
-            else:
-                switch_pin(OUT_PINS[item])
-        except ValueError:
-            pass
-        except KeyError:
-            pass
-        stdscr.refresh()
-
-    stdscr.getkey()
+def main():
+    """Main function"""
+    #TODO
+    root = tk.Tk()
+    MainWindow(root)
+    root.mainloop()
 
 def shutdown():
     """Shutdown commands"""
@@ -387,14 +282,5 @@ if __name__ == '__main__':
     signal.signal(signal.SIGHUP, handler)
     signal.signal(signal.SIGINT, handler)
     signal.signal(signal.SIGTERM, handler)
-    try:
-        print("Starting up curses wrapper...")
-        curses.wrapper(main)
-        print("Shutting down after a hard day...")
-        shutdown()
-    except KeyboardInterrupt:
-        print("Shutting down...")
-        shutdown()
-    except SystemExit as ex:
-        print(ex)
-        shutdown()
+
+    main()
