fb119c1fef84d95091b8de6a44c24c5249b4017b
Dylan Armitage <d.armitage89@gmail.com>
2017-11-09 13:39:07 -0800

    Should be all the stuff for Sender.py...

--- Sender.py
+++ Sender.py
@@ -2,139 +2,270 @@
 # coding=UTF-8
 """Module to communicate with GRBL via serial"""
 
+
 __author__ = "Dylan Armitage"
 __email__ = "d.armitage89@gmail.com"
 
+# This script is heavily influenced by bCNC
+# https://github.com/vlachoudis/bCNC
+
+####---- Import ----####
+from multiprocessing import Process, Queue
+
+import sys
+import time
+import datetime
+import serial
 
 
-####---- GRBL Interface codes ----####
-# More info: https://github.com/gnea/grbl/wiki/Grbl-v1.1-Interface
-## dict(alarm_code : (alarm message, alarm description))
-ALARM_CODES = {1:("Hard limit",
-                  "Hard limit has been triggered."
-                  " Machine position is likely lost due to sudden halt."
-                  " Re-homing is highly recommended."),
-               2:("Soft limit",
-                  "Soft limit alarm."
-                  " G-code motion target exceeds machine travel."
-                  " Machine position retained."
-                  " Alarm may be safely unlocked."),
-               3:("Abort during cycle",
-                  "Reset while in motion."
-                  " Machine position is likely lost due to sudden halt."
-                  " Re-homing is highly recommended."),
-               4:("Probe fail",
-                  "Probe fail."
-                  " Probe is not in the expected initial state before"
-                  " starting probe cycle when G38.2 and G38.3 is not"
-                  " triggered and G38.4 and G38.5 is triggered."),
-               5:("Probe fail",
-                  "Probe fail."
-                  " Probe did not contact the workpiece within the"
-                  " programmed travel for G38.2 and G38.4."),
-               6:("Homing fail",
-                  "Homing fail. The active homing cycle was reset."),
-               7:("Homing fail",
-                  "Homing fail."
-                  " Safety door was opened during homing cycle."),
-               8:("Homing fail",
-                  "Homing fail."
-                  " Pull off travel failed to clear limit switch."
-                  " Try increasing pull-off setting or check wiring."),
-               9:("Homing fail",
-                  "Homing fail. Could not find limit switch within"
-                  " search distances. Try increasing max travel,"
-                  " decreasing pull-off distance, or check wiring.")}
-## dict(error_code : (error_message, error_description))
-ERROR_CODES = {1:("Expected command letter",
-                  "G-code words consist of a letter and a value."
-                  " Letter was not found."),
-               2:("Bad number format",
-                  "Missing the expected G-code word value or numeric"
-                  " value format is not valid."),
-               3:("Invalid statement",
-                  "Grbl '$' system command was not recognized"
-                  " or supported."),
-               4:("Value < 0",
-                  "Negative value received for an expected positive value."),
-               5:("Setting disabled",
-                  "Homing cycle failure."
-                  " Homing is not enabled via settings."),
-               6:("Value < 3 usec",
-                  "Minimum step pulse time must be greater than 3usec."),
-               7:("EEPROM read fail. Using defaults",
-                  "An EEPROM read failed."
-                  " Auto-restoring affected EEPROM to default values."),
-               8:("Not idle",
-                  "Grbl '$' command cannot be used unless Grbl is IDLE."
-                  " Ensures smooth operation during a job."),
-               9:("G-code lock",
-                  "G-code commands are locked out during alarm or"
-                  " jog state."),
-               10:("Homing not enabled",
-                   "Soft limits cannot be enabled without homing"
-                   " also enabled."),
-               11:("Line overflow",
-                   "Max characters per line exceeded."
-                   " Received command line was not executed."),
-               12:("Step rate > 30kHz",
-                   "Grbl '$' setting value cause the step rate to"
-                   " exceed the maximum supported."),
-               13:("Check Door",
-                   "Safety door detected as opened and door"
-                   " state initiated."),
-               14:("Line length exceeded",
-                   "Build info or startup line exceeded EEPROM"
-                   " line length limit. Line not stored."),
-               15:("Travel exceeded",
-                   "Jog target exceeds machine travel."
-                   " Jog command has been ignored."),
-               16:("Invalid jog command",
-                   "Jog command has no '=' or contains prohibited g-code."),
-               17:("Setting disabled",
-                   "Laser mode requires PWM output."),
-               20:("Unsupported command",
-                   "Unsupported or invalid g-code command found in block."),
-               21:("Modal group violation",
-                   "More than one g-code command from same modal"
-                   " group found in block."),
-               22:("Undefined feed rate",
-                   "Feed rate has not yet been set or is undefined."),
-               23:("Invalid gcode ID:23",
-                   "G-code command in block requires an integer value."),
-               24:("Invalid gcode ID:24",
-                   "More than one g-code command that requires"
-                   " axis words found in block."),
-               25:("Invalid gcode ID:25",
-                   "Repeated g-code word found in block."),
-               26:("Invalid gcode ID:26",
-                   "No axis words found in block for g-code command"
-                   " or current modal state which requires them."),
-               27:("Invalid gcode ID:27",
-                   "Line number value is invalid."),
-               28:("Invalid gcode ID:28",
-                   "G-code command is missing a required value word."),
-               29:("Invalid gcode ID:29",
-                   "G59.x work coordinate systems are not supported."),
-               30:("Invalid gcode ID:30",
-                   "G53 only allowed with G0 and G1 motion modes."),
-               31:("Invalid gcode ID:31",
-                   "Axis words found in block when no command or"
-                   " current modal state uses them."),
-               32:("Invalid gcode ID:32",
-                   "G2 and G3 arcs require at least one"
-                   " in-plane axis word."),
-               33:("Invalid gcode ID:33",
-                   "Motion command target is invalid."),
-               34:("Invalid gcode ID:34",
-                   "Arc radius value is invalid."),
-               35:("Invalid gcode ID:35",
-                   "G2 and G3 arcs require at least one"
-                   " in-plane offset word."),
-               36:("Invalid gcode ID:36",
-                   "Unused value words found in block."),
-               37:("Invalid gcode ID:37",
-                   "G43.1 dynamic tool length offset is not assigned"
-                   " to configured tool length axis."),
-               38:("Invalid gcode ID:38",
-                   "Tool number greater than max supported value.")}
+#from GrblCodes import ALARM_CODES, ERROR_CODES
+
+# Global variables
+SERIAL_TIMEOUT = 0.1 # seconds
+SERIAL_POLL = 0.25 # seconds
+G_POLL = 10 # seconds
+RX_BUFFER_SIZE = 128 # bytes
+
+class Sender(object):
+    """Class that controls access to GRBL"""
+    # pylint: disable=too-many-instance-attributes
+    # If we get many more than 8 though...
+    def __init__(self):
+        self.log = Queue() # What is returned from GRBL
+        self.queue = Queue() # What to send to GRBL
+        self.serial = None
+        self.process = None
+
+        self.running = False
+        self._stop = False # Set to True to stop current run
+        self._pause = False # Is it currently paused?
+        # The reason _stop and _pause represent different conditions is
+        # because of the way GRBL handles stopping/pausing.
+        # If GRBL is stopped, GRBL clears its queue, so we no longer have a
+        # reliable method of continuation. So by setting _stop = True, we can
+        # clear our queue as well as stop GRBL.
+        # However, GRBL will immediately pause if sent "!" at any time. It can
+        # be resumed from the exact same position by sending "~", so there is
+        # only a need to tracking whether GRBL is paused or not, as the queue
+        # will remain the same.
+        self._sum_command_lens = 0
+
+    def serial_io(self):
+        """Process to perform I/O on GRBL"""
+        # pylint: disable=too-many-branches,too-many-statements
+        # Probably should split this up...
+        command_lens = [] # length of piplined commands
+        command_pipe = [] # pipelined commands
+        wait = False # wait for commands to complete, aka status change to Idle
+        to_send = None # Next command to send
+        status = False # wait for status report (<...> from ? command)
+        t_report = t_state = time.time() # when ? or $G was sent last
+
+        while self.process:
+            t_curr = time.time()
+            if t_curr-t_report > SERIAL_POLL:
+                self.serial.write(b"?")
+                status = True
+                t_report = t_curr
+
+            if (to_send is None # nothing to send
+                    and not wait # and not waiting
+                    and not self._pause # and not paused
+                    and self.queue.qsize() > 0): # and stuff in the queue
+                to_send = self.queue.get_nowait()
+                if isinstance(to_send, tuple):
+                    if to_send[0] == "WAIT":
+                        wait = True
+                    to_send = None
+                elif isinstance(to_send, (str, unicode)):
+                    to_send += "\n"
+
+                if to_send is not None:
+                    if isinstance(to_send, unicode):
+                        to_send = to_send.encode("ascii", "replace")
+                command_pipe.append(to_send)
+                command_lens.append(len(to_send))
+
+            # Receive anything waiting
+            if self.serial.inWaiting() or to_send is None:
+                try:
+                    line = str(self.serial.readline()).strip()
+                except BaseException: # Queue is likely correupted, disconnect
+                    self.log.put(("Received",
+                                  str(sys.exc_info()[1]),
+                                  datetime.datetime()))
+                    self.empty_queue()
+                    self.close_serial()
+                    return
+                if not line:
+                    # Nothing actually received
+                    pass
+                elif line[0] == "<": # There's a status message!
+                    if not status: # We weren't expecting one
+                        self.log.put(("Received", line))
+                    # Status was True then
+                    status = False
+                    fields = line[1:-1].split("|")
+                    # Machine is idle, continue!
+                    if wait and fields[0] in ("Idle", "Check"):
+                        wait = False
+                elif line[0] == "[":
+                    self.log.put(("Received", line))
+                elif "error:" in line or "ALARM:" in line:
+                    self.log.put(("ERROR", line))
+                    if command_lens:
+                        del command_lens[0]
+                    if command_pipe:
+                        del command_pipe[0]
+                    if self.running:
+                        self._stop = True
+                elif line.find("ok") >= 0: # It was okay!
+                    self.log.put(("OK", line))
+                    if command_lens:
+                        del command_lens[0]
+                    if command_pipe:
+                        del command_pipe[0]
+                elif line[:4] == "Grbl": # Grbl was reset
+                    t_state = time.time()
+                    self.log.put(("Received", line))
+                    self._stop = True
+                    # It would be pointless to keep the current queue
+                    del command_lens[:]
+                    del command_pipe[:]
+            # Received a command to stop
+            if self._stop:
+                self.empty_queue()
+                to_send = None
+                self.log.put(("Cleared", ""))
+            # If there's stuff to actually send...
+            if (to_send is not None
+                    and sum(command_lens) < RX_BUFFER_SIZE):
+                self._sum_command_lens = sum(command_lens)
+                self.serial.write(bytes(to_send))
+                self.log.put(("Buffered", to_send))
+                to_send = None
+                if not self.running and t_curr-t_state > G_POLL:
+                    to_send = b"$G\n"
+                    command_pipe.append(to_send)
+                    command_lens.append(len(to_send))
+                    t_state = t_curr
+
+
+    def open_serial(self, device):
+        """Open serial port"""
+        self.serial = serial.serial_for_url(device,
+                                            baudrate=115200,
+                                            bytesize=serial.EIGHTBITS,
+                                            parity=serial.PARITY_NONE,
+                                            stopbits=serial.STOPBITS_ONE,
+                                            timeout=SERIAL_TIMEOUT,
+                                            xonxoff=False,
+                                            rtscts=False)
+        # Toggle DTR to reset the arduino
+        try:
+            self.serial.setDTR(0)
+            time.sleep(1)
+            self.serial.setDTR(1)
+            time.sleep(1)
+        except IOError:
+            pass
+        self.serial.write(b"\n\n")
+        self.process = Process(target=self.serial_io)
+        self.process.start()
+        return True
+
+    def close_serial(self):
+        """Close serial port"""
+        if self.serial is None:
+            return
+        try:
+            self.stop_run()
+        except BaseException:
+            pass
+        self.process.join()
+        self.process = None
+        try:
+            self.serial.close()
+        except BaseException:
+            raise
+        self.serial = None
+        return True
+
+    def stop_run(self):
+        """Stop the current run of Gcode"""
+        self.pause()
+        self._stop = True
+        self.purge_grbl()
+
+    def purge_grbl(self):
+        """Purge the buffer of grbl"""
+        self.serial.write(b"!") # Immediately pause
+        self.serial.flush()
+        time.sleep(1)
+        self.soft_reset()
+        self.unlock()
+        self.run_ended()
+
+    def run_ended(self):
+        """Called when run is finished"""
+        if self.running:
+            self.log.put(("Run ended", datetime.datetime.now()))
+
+        self._pause = False
+        self.running = False
+
+    def soft_reset(self):
+        """Send GRBL reset command"""
+        if self.serial:
+            self.serial.write(b"\030")
+
+    def unlock(self):
+        """Send GRBL unlock command"""
+        self.send_gcode("$X")
+
+    def home(self):
+        """Send GRBL home command"""
+        self.send_gcode("$H")
+
+    def send_gcode(self, command):
+        """Send GRBL a Gcode/command line"""
+        # Do nothing if not actually up
+        if self.serial and not self.running:
+            if isinstance(command, tuple):
+                self.queue.put(command)
+            else:
+                self.queue.put(command+"\n")
+
+    def empty_queue(self):
+        """Clear the queue"""
+        while self.queue.qsize() > 0:
+            try:
+                self.queue.get_nowait()
+            except BaseException:
+                break
+
+    def init_run(self):
+        """Initialize a gcode run"""
+        self._pause = False
+        self.running = True
+        self.empty_queue()
+        time.sleep(1) # Give everything a bit of time
+
+    def pause(self):
+        """Pause run"""
+        if self.serial is None:
+            return
+        if self._pause:
+            self.resume()
+        else:
+            self.serial.write(b"!")
+            self.serial.flush()
+            self._pause = True
+
+    def resume(self):
+        """Resume a run"""
+        if self.serial is None:
+            return
+        self.serial.write(b"~")
+        self.serial.flush()
+        self._pause = False
