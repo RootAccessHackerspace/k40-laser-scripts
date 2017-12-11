6f39c9863bcc966ac54d6f5b347ba0a77a19a957
Dylan Armitage <d.armitage89@gmail.com>
2017-12-11 15:48:28 -0800

    REFACTOR Gcodeparser into a class with methods
    Really, if it's going to be like that, it might as well contain all
    logic + data in a single object.

--- GcodeParser.py
+++ GcodeParser.py
@@ -9,29 +9,64 @@ __email__ = "d.armitage89@gmail.com"
 ####---- Imports ----####
 from pygcode import Line, GCodeLinearMove
 
-def bounding_box(gcode_file):
-    """Take in file of gcode, return tuples of min/max bounding values"""
-    lines = [Line(line) for line in gcode_file]
-    params = [p.get_param_dict() for p in lines if p.word == "G01"]
-    x_pos = [p["X"] for p in params]
-    y_pos = [p["Y"] for p in params]
-    return ((min(x_pos),min(y_pos)), (max(x_pos),max(y_pos)))
+class GcodeFile(object):
+    """A file of gcode"""
+    def __init__(self, gcode_file=[]):
+        self.file = gcode_file
+        self.gcode = []
+        self.extrema = dict(X=(None, None), Y=(None, None),
+                            UL=(None, None), DR=(None, None),
+                           )
+        self.mids = dict(X=None, Y=None)
+        if self.file and not self.gcode:
+            self.__convert_gcode_internal()
 
-def box_gcode(min_xy, max_xy):
-    """Take in min/max coordinate tuples, return G0 commands to bound it"""
-    gcode = []
-    gcode.append(GCodeLinearMove(X=min_xy[0], Y=min_xy[1]))
-    gcode.append(GCodeLinearMove(X=max_xy[0], Y=min_xy[1]))
-    gcode.append(GCodeLinearMove(X=max_xy[0], Y=max_xy[1]))
-    gcode.append(GCodeLinearMove(X=min_xy[0], Y=max_xy[1]))
-    gcode.append(GCodeLinearMove(X=min_xy[0], Y=min_xy[1]))
-    # Convert from GCodeLinearMove class to string
-    gcode = [str(line) for line in gcode]
-    return gcode
+    def add_file(self, gcode_file):
+        """Read in a file of Gcode"""
+        self.file = gcode_file
+        self.__convert_gcode_internal()
 
-def mid_gcode(min_xy, max_xy):
-    """Take in min/max coord tuples, return G0 to go to midpoint"""
-    mid_x = float((min_xy[0]+max_xy[0])/2)
-    mid_y = float((min_xy[1]+max_xy[1])/2)
-    return str(GCodeLinearMove(X=mid_x, Y=mid_y))
+    def __convert_gcode_internal(self):
+        """Convert gcode into format that can be easily manipulated"""
+        for line in self.file:
+            self.gcode.append(Line(line))
+
+    def bounding_box(self):
+        """Take in file of gcode, return tuples of min/max bounding values"""
+        if self.file or self.gcode:
+            logger.error("Load file first")
+            return None
+        if (None, None) in self.extrema.values():
+            params = [p.get_param_dict()
+                      for p in self.gcode
+                      if p.word == "G01"]
+            x_pos = [p["X"] for p in params]
+            y_pos = [p["Y"] for p in params]
+            self.extrema["X"] = (min(x_pos), max(x_pos))
+            self.extrema["Y"] = (min(y_pos), max(y_pos))
+            self.extrema["UL"] = (self.extrema["X"][0],
+                                  self.extrema["Y"][0])
+            self.extrema["DR"] = (self.extrema["X"][1],
+                                  self.extrema["Y"][1])
+        return (self.extrema["UL"], self.extrema["DR"])
+
+    def box_gcode(self):
+        """Return G0 commands to bound gcode file"""
+        gcode = []
+        gcode.append(GCodeLinearMove(X=self.extrema["X"][0],
+                                     Y=self.extrema["Y"][0])) #UL
+        gcode.append(GCodeLinearMove(X=self.extrema["X"][0],
+                                     Y=self.extrema["Y"][1])) #UR
+        gcode.append(GCodeLinearMove(X=self.extrema["X"][1],
+                                     Y=self.extrema["Y"][1])) #DR
+        gcode.append(GCodeLinearMove(X=self.extrema["X"][1],
+                                     Y=self.extrema["Y"][0])) #DL
+        gcode.append(GCodeLinearMove(X=self.extrema["X"][0],
+                                     Y=self.extrema["Y"][0])) #UL cycle
+        # Convert from GCodeLinearMove class to string
+        gcode = [str(line) for line in gcode]
+        return gcode
+
+    def mid_gcode(min_xy, max_xy):
+        raise NotImplemented
 
